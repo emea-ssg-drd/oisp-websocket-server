1e8a66774782426e8c1d2ca201fabc42683f7144
Oguzcan Kirmemis <oguzcan.kirmemis@gmail.com>
2019-05-22 04:03:51 +0200

    Added circleci config file, enforced linter on build
    Signed-off-by: Oguzcan Kirmemis <oguzcan.kirmemis@gmail.com>

--- config.js
+++ config.js
@@ -18,46 +18,46 @@
 // Gets the config for the configName from the OISP_WEBSOCKET_SERVER_CONFIG environment variable
 // Returns empty object if the config can not be found
 var getOISPConfig = (function () {
-	if (!process.env.OISP_WEBSOCKET_SERVER_CONFIG) {
-		console.log("Root config environment variable (OISP_WEBSOCKET_SERVER_CONFIG) is missing...");
-		return function () { return {}; };
-	}
-	var websocketServerConfig = JSON.parse(process.env.OISP_WEBSOCKET_SERVER_CONFIG);
+    if (!process.env.OISP_WEBSOCKET_SERVER_CONFIG) {
+        console.log("Root config environment variable (OISP_WEBSOCKET_SERVER_CONFIG) is missing...");
+        return function () { return {}; };
+    }
+    var websocketServerConfig = JSON.parse(process.env.OISP_WEBSOCKET_SERVER_CONFIG);
 
-	var resolveConfig = function (config, stack) {
-		if (!stack) {
-			stack = ["OISP_WEBSOCKET_SERVER_CONFIG"];
-		}
-		for (var property in config) {
-			if (typeof config[property] === "string" &&
+    var resolveConfig = function (config, stack) {
+        if (!stack) {
+            stack = ["OISP_WEBSOCKET_SERVER_CONFIG"];
+        }
+        for (var property in config) {
+            if (typeof config[property] === "string" &&
 					(config[property].substring(0,2) === "@@" || config[property].substring(0,2) === "%%")) {
-				var configName = config[property].substring(2, config[property].length);
-				if (!process.env[configName]) {
-					console.log("Config environment variable (" + configName + ") is missing...");
-					config[property] = {};
-				} else if (stack.indexOf(configName) !== -1) {
-					console.log("Detected cyclic reference in config decleration: " + configName + ", stopping recursion...");
-					config[property] = {};
-				} else {
-					config[property] = JSON.parse(process.env[configName]);
-					stack.push(configName);
-					resolveConfig(config[property], stack);
-					stack.pop();
-				}
-			}
-		}
-	};
+                var configName = config[property].substring(2, config[property].length);
+                if (!process.env[configName]) {
+                    console.log("Config environment variable (" + configName + ") is missing...");
+                    config[property] = {};
+                } else if (stack.indexOf(configName) !== -1) {
+                    console.log("Detected cyclic reference in config decleration: " + configName + ", stopping recursion...");
+                    config[property] = {};
+                } else {
+                    config[property] = JSON.parse(process.env[configName]);
+                    stack.push(configName);
+                    resolveConfig(config[property], stack);
+                    stack.pop();
+                }
+            }
+        }
+    };
 
-	resolveConfig(websocketServerConfig);
+    resolveConfig(websocketServerConfig);
 
-	return function(configName) {
-			if (!websocketServerConfig[configName])
-				return {};
-			else {
-				console.log(configName + " is set to: " + JSON.stringify(websocketServerConfig[configName]));
-				return websocketServerConfig[configName];
-			}
-		};
+    return function(configName) {
+        if (!websocketServerConfig[configName])
+        {return {};}
+        else {
+            console.log(configName + " is set to: " + JSON.stringify(websocketServerConfig[configName]));
+            return websocketServerConfig[configName];
+        }
+    };
 })();
 
 var postgres_config = getOISPConfig("postgresConfig"),
@@ -70,29 +70,29 @@ var postgres_config = getOISPConfig("postgresConfig"),
 // Get replica information from the postgres config,
 // Done this way to avoid compatibility problems with other services
 var	postgresReadReplicas = [],
-	postgresWriteConf = {};
+    postgresWriteConf = {};
 
 if (postgres_config.readReplicas) {
-	postgresReadReplicas = postgres_config.readReplicas;
+    postgresReadReplicas = postgres_config.readReplicas;
 } else if (postgres_config.readHostname) {
-	postgresReadReplicas.push({
-		host: postgres_config.readHostname,
-		port: postgres_config.readPort,
-		username: postgres_config.readUsername,
-		password: postgres_config.readPassword
-	});
+    postgresReadReplicas.push({
+        host: postgres_config.readHostname,
+        port: postgres_config.readPort,
+        username: postgres_config.readUsername,
+        password: postgres_config.readPassword
+    });
 } else {
-	// Use default db config as read
-	postgresReadReplicas.push({});
+    // Use default db config as read
+    postgresReadReplicas.push({});
 }
 
 if (postgres_config.writeHostname) {
-	postgresWriteConf = {
-		host: postgres_config.writeHostname,
-		port: postgres_config.writePort,
-		username: postgres_config.writeUsername,
-		password: postgres_config.writePassword,
-	};
+    postgresWriteConf = {
+        host: postgres_config.writeHostname,
+        port: postgres_config.writePort,
+        username: postgres_config.writeUsername,
+        password: postgres_config.writePassword,
+    };
 }
 
 var config = {
@@ -104,10 +104,10 @@ var config = {
             host: postgres_config.hostname,
             port: postgres_config.port,
             dialect: 'postgres',
-			replication: {
-				read: postgresReadReplicas,
-				write: postgresWriteConf
-			},
+            replication: {
+                read: postgresReadReplicas,
+                write: postgresWriteConf
+            },
             pool: {
                 max: 12,
                 min: 0,
@@ -141,9 +141,9 @@ var config = {
 };
 
 if (process.env.DASHBOARD_SERVICE_HOST) {
-	config.ws.externalAddress = config.ws.serverAddress + '.websocket-server';
+    config.ws.externalAddress = config.ws.serverAddress + '.websocket-server';
 } else {
-	config.ws.externalAddress = config.ws.serverAddress;
+    config.ws.externalAddress = config.ws.serverAddress;
 }
 config.ws.externalPort = config.ws.port;
 
